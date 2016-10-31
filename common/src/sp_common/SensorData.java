3081c324a9f6c747f81cb4f8f68c961b76f0b766
Arne <sujomear@arcor.de>
2016-10-31 23:52:06 +0100

    added deep copy clone()

--- common/src/sp_common/SensorData.java
+++ common/src/sp_common/SensorData.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 /**
  * This class encapsulates all data for a single sensor event
  */
-public class SensorData implements Serializable {
+public class SensorData implements Serializable, Cloneable {
     public SensorData(SensorType sensorType, float[] data, long timestamp, int accuracy) {
         this.sensorType = sensorType;
         this.data = data;
@@ -23,6 +23,17 @@ public class SensorData implements Serializable {
     public String toString() {
         // print the data as csv
         return Arrays.toString(data).replaceAll("[ \\[\\]]", "") + "," + timestamp + "," + accuracy;
+    }
+
+    @Override
+    protected SensorData clone() throws CloneNotSupportedException {
+        // have to do it this way to ensure proper initialization
+        SensorData cloned = (SensorData) super.clone();
+        cloned.sensorType = sensorType;
+        cloned.data = data.clone();
+        cloned.timestamp = timestamp;
+        cloned.accuracy = accuracy;
+        return cloned;
     }
 
     public SensorType sensorType;
