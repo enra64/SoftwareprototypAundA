d0bb2f24590a3e5ba06e45f8d33d4bd550114daa
Arne <sujomear@arcor.de>
2016-11-01 18:14:08 +0100

    added rudimentary mouse control

--- Server/src/MouseControl.java
+++ Server/src/MouseControl.java
@@ -1,18 +1,51 @@
 import sp_common.SensorData;
 
-/**
- * Created by arne on 10/31/16.
- */
-public class MouseControl implements TcpServer.ServerThread.OnUpdateListener {
+import java.awt.*;
+import java.util.Arrays;
 
-    public static void main(String args[]) {
+
+public class MouseControl implements TcpServer.ServerThread.OnUpdateListener {
+    private float[] mLinearAcceleration = new float[3];
+    private float[] mGravity = new float[3];
+    private final float ALPHA = 0.8f;
+    private Robot mRobot;
+
+    public static void main(String args[]) throws AWTException {
         MouseControl robot = new MouseControl();
         TcpServer.ServerThread server = new TcpServer.ServerThread(robot);
         server.start();
     }
 
+    public MouseControl() throws AWTException {
+        mRobot = new Robot();
+
+        mRobot.setAutoDelay(40);
+        mRobot.setAutoWaitForIdle(true);
+    }
+
     @Override
     public void onUpdate(SensorData sensorData) {
+        // Isolate the force of gravity with the low-pass filter.
+        mGravity[0] = ALPHA * mGravity[0] + (1 - ALPHA) * sensorData.data[0];
+        mGravity[1] = ALPHA * mGravity[1] + (1 - ALPHA) * sensorData.data[1];
+        mGravity[2] = ALPHA * mGravity[2] + (1 - ALPHA) * sensorData.data[2];
 
+        // Remove the gravity contribution with the high-pass filter.
+        mLinearAcceleration[0] = sensorData.data[0] - mGravity[0];
+        mLinearAcceleration[1] = sensorData.data[1] - mGravity[1];
+        mLinearAcceleration[2] = sensorData.data[2] - mGravity[2];
+
+        System.out.print("\r" + Arrays.toString(mLinearAcceleration));
+
+        Point currentLocation = MouseInfo.getPointerInfo().getLocation();
+
+        // left
+        if(mLinearAcceleration[0] < -1){
+            mRobot.mouseMove(currentLocation.x + 1, currentLocation.y);
+        }
+        // right
+        else if (mLinearAcceleration[0] > 1){
+            mRobot.mouseMove(currentLocation.x - 1, currentLocation.y);
+        }
     }
 }
