01e6bd72a9b0a35dd1819b7f25bd7b897dac82dc
Arne <sujomear@arcor.de>
2016-11-01 10:09:18 +0100

    better SensorData.clone(), comments

--- common/src/sp_common/SensorData.java
+++ common/src/sp_common/SensorData.java
@@ -1,21 +1,24 @@
 package sp_common;
 
-import sun.management.Sensor;
-
 import java.io.Serializable;
 import java.util.Arrays;
 
 /**
  * This class encapsulates all data for a single sensor event
  */
+@SuppressWarnings("WeakerAccess")
 public class SensorData implements Serializable, Cloneable {
+    public SensorType sensorType;
+    public float[] data;
+    public long timestamp;
+    public int accuracy;
+
     public SensorData(SensorType sensorType, float[] data, long timestamp, int accuracy) {
         this.sensorType = sensorType;
         this.data = data;
         this.timestamp = timestamp;
         this.accuracy = accuracy;
     }
-
     public SensorData() {
     }
 
@@ -25,19 +28,26 @@ public class SensorData implements Serializable, Cloneable {
         return Arrays.toString(data).replaceAll("[ \\[\\]]", "") + "," + timestamp + "," + accuracy;
     }
 
+    /**
+     * Returns a properly cloned SensorData object
+     */
     @Override
-    public SensorData clone() throws CloneNotSupportedException {
+    public SensorData clone() {
         // have to do it this way to ensure proper initialization
-        SensorData cloned = (SensorData) super.clone();
-        cloned.sensorType = sensorType;
-        cloned.data = data.clone();
-        cloned.timestamp = timestamp;
-        cloned.accuracy = accuracy;
+        SensorData cloned = null;
+
+        // try { clone() } to avoid throws in signature
+        try {
+            cloned = (SensorData) super.clone();
+            cloned.sensorType = sensorType;
+            cloned.data = data.clone();
+            cloned.timestamp = timestamp;
+            cloned.accuracy = accuracy;
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        // this cannot be reached, as we inherit directly from Object
         return cloned;
     }
-
-    public SensorType sensorType;
-    public float[] data;
-    public long timestamp;
-    public int accuracy;
 }
